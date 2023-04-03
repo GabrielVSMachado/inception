0cbebb5abb404704677282d027fe3cf7c3603d59
Gabriel Vitor Sales Machado <gabriel.machado@loft.com.br>
2023-04-03 20:19:09 -0300

    feat: implement config mariadb

---
+++ srcs/requirements/mariadb/mariadb_entrypoint.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+set -e
+
+#rise service
+mariadb-server start
+
+CHARACTERS='a-zA-Z0-9"!@#$%&*()-_=+[],.?^`{}'
+
+MARIADB_RANDOM_PASSWORD=$(tr -dc $CHARACTERS </dev/urandom | head -c 80)
+
+mariadb-secure-installation <<EOF
+  $MARIADB_RANDOM_PASSWORD
+  Y
+  n
+  Y
+  Y
+  Y
+  Y
+EOF
+
+
+mariadb \
+  -u root \
+  -e "CREATE USER 'gvitor-s'@'localhost' IDENTIFIED BY '$MARIADB_USER_PASSWORD'"
+
+mariadb \
+  -u root \
+  -e "GRANT ALL PRIVILEGES ON mysql.* TO 'gvitor-s'@'localhost'; FLUSH PRIVILEGES"
+
+mariadb \
+  -u root \
+  -e "FLUSH PRIVILEGES; \
+      ALTER USER 'root'@'localhost' IDENTIFIED BY '$MARIADB_RANDOM_PASSWORD'"
+
+exec mariadbd-server restart --user=mysql
