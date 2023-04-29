43c39ad7ee4d2bfe26afe7749e75cabd8176d016
gvitor-s <gvitor-s@student.42sp.org.br>
2023-04-29 19:15:36 -0300

    feat: setup bind volumes to wordpress and nginx

--- Makefile
+++ Makefile
@@ -1,14 +1,23 @@
 COMMAND = docker-compose -f ./srcs/docker-compose.yml
+USER_DIR = /home/gvitor-s
+VOLUME_PATH_WORDPRESS = $(USER_DIR)/data/wordpress
+VOLUME_PATH_MARIADB = $(USER_DIR)/data/mariadb
+override RM = rm -rf
 
-all:
+all: | $(USER_DIR)
 	sudo $(COMMAND) up --build
-	
+
+$(USER_DIR):
+	sudo mkdir -p $(USER_DIR) && sudo chown -R user42:user42 $(USER_DIR)
+	mkdir -p $(VOLUME_PATH_MARIADB)
+	mkdir -p $(VOLUME_PATH_WORDPRESS)
 
 clean:
 	$(COMMAND) down
 
 fclean:
 	$(COMMAND) down --rmi all -v
+	sudo $(RM) $(USER_DIR)
 
 re: fclean all
 
--- srcs/requirements/wordpress/entrypoint.sh
+++ srcs/requirements/wordpress/entrypoint.sh
@@ -4,30 +4,36 @@ WORDPRESS_DIR_PATH=wordpress
 WORDPRESS_CONFIG_PATH="$WORDPRESS_DIR_PATH/wp-config.php"
 
 
-if [[ $(ls /www/wordpress | wc -l) -eq 0 ]]; then
-  wp core download --path=$WORDPRESS_DIR_PATH
+setup() {
 
-  wp config create \
-    --dbname=wordpress \
-    --dbuser=$MARIADB_USER \
-    --dbpass=$MARIADB_USER_PASSWORD \
-    --dbhost=db \
-    --path=$WORDPRESS_DIR_PATH
+wp core download --path=$WORDPRESS_DIR_PATH
 
-  wp db create --path=$WORDPRESS_DIR_PATH
+wp config create \
+  --dbname=wordpress \
+  --dbuser=$MARIADB_USER \
+  --dbpass=$MARIADB_USER_PASSWORD \
+  --dbhost=db \
+  --path=$WORDPRESS_DIR_PATH
 
-  wp core install \
-    --url=localhost \
-    --title="42sp-inception" \
-    --admin_user=$MARIADB_USER \
-    --admin_password=$MARIADB_USER_PASSWORD \
-    --admin_email="$MARIADB_USER@student.42sp.org.br" \
-    --path=$WORDPRESS_DIR_PATH
+wp db create --path=$WORDPRESS_DIR_PATH
 
-  sed -i '21i define("WP_HOME", "https://" . $_SERVER["HTTP_HOST"]. "/");\n' \
-    $WORDPRESS_CONFIG_PATH
-  sed -i '22i define("WP_SITEURL", "https://" .$_SERVER["HTTP_HOST"]. "/");\n' \
-    $WORDPRESS_CONFIG_PATH
-fi
+wp core install \
+  --url=localhost \
+  --title="42sp-inception" \
+  --admin_user=$MARIADB_USER \
+  --admin_password=$MARIADB_USER_PASSWORD \
+  --admin_email="$MARIADB_USER@student.42sp.org.br" \
+  --path=$WORDPRESS_DIR_PATH
+
+sed -i '21i define("WP_HOME", "https://" . $_SERVER["HTTP_HOST"]. "/");\n' \
+  $WORDPRESS_CONFIG_PATH
+sed -i '22i define("WP_SITEURL", "https://" .$_SERVER["HTTP_HOST"]. "/");\n' \
+  $WORDPRESS_CONFIG_PATH
+
+}
+
+[ $(ls /www/wordpress | wc -l) -eq 0 ] && setup
+
+echo "Setup completed !!!"
 
 exec php-fpm8 --nodaemonize --force-stderr
